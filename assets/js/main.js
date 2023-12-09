e7e4d3d8b8620782d4e9da4096f8f61d2a200bf6
Macos <vusalarg@gmail.com>
2023-12-10 00:24:26 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+//math//
+let a = 23;
+let b = 32;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(a,b);
+
+
+
+//bd calculate//
+let birthYear = 1988;
+let futureYear  = 2026;
+let age  = futureYear - birthYear;
+console.log("I will be either " + (age - 1) + " or " + age + " in " + futureYear);
+
+
+
+//future//
+let numOfChildren = 2;
+let housbandName = "Jhon";
+let geoLocation = "New York";
+let job = "a CEO";
+let future_Year  = 2026;
+console.log("You will be " + job + " in " + geoLocation + " in " + future_Year + " and married to " + housbandName + " with " + numOfChildren + " kids." );
